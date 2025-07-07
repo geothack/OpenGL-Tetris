b0675b63a5b378bab39adcfe2632e66dfc97a82d
geothack <146097573+geothack@users.noreply.github.com>
2025-07-07 18:37:27 +0100

    Premake Build Ready

---
+++ premake5.lua
@@ -0,0 +1,22 @@
+
+workspace "Breakout"
+    configurations {"Debug","Release"}
+
+project "Breakout"
+    kind "ConsoleApp"
+
+    language "C++"
+    cppdialect "C++latest"
+    targetdir "bin/%{cfg.buildcfg}"
+
+    architecture ("x64")
+
+   files { "src/Game/**.h", "src/Game/**.cpp" }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
---
+++ src/Game/main.cpp
@@ -0,0 +1,9 @@
+
+
+#include <print>
+
+
+int main()
+{
+    std::println("Hello World");
+}
